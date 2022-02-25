3a5bf9ba5ec677c64ca07cd437ab73d418f41305
even <even@MSI.localdomain>
2022-02-25 18:46:12 +0100

    myquotev2 upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/EvenBF/myquotev2
+
+go 1.13
+
+require rsc.io/quote v1.5.2
---
+++ pakke/pakke.go
@@ -0,0 +1,8 @@
+package pakke
+
+import "rsc.io/quote"
+
+func TestQuote() string {
+	return quote.Glass() + "\n" + quote.Go() + "\n" + quote.Opt() + "\n" + quote.Hello();
+
+}
